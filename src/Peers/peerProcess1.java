5140a7a0a813196d3a70a7623f7060df18c3bc7e
Animesh Srivastava <animeshsrivastava@Animeshs-MacBook-Air.local>
2021-03-06 13:37:16 -0500

    Peer package added

---
+++ src/Peers/peerProcess1.java
@@ -0,0 +1,16 @@
+package Peers;
+import File.CommonCfg;
+import File.PeerCfg;
+public class peerProcess1 {
+    public static String FileName;
+    private long FileSize;
+    private long PieceSize;
+    private int TotalPieces;
+    private int peerID;
+    private int port;
+    private boolean hasFile;
+
+
+
+
+}
