558e4808320aa3aa4df707a858627cc6114f89de
maitreyeegupte <36708263+maitreyeegupte@users.noreply.github.com>
2021-03-06 10:14:18 -0500

    Add files via upload

---
+++ src/Common/HandShake_Message.java
@@ -0,0 +1,15 @@
+public class HandShake_Message {
+    byte[] Msg = new byte[32];
+    String Header = "P2PFILESHARINGPROJ";
+    String Zero_Bits = "0000000000";
+    int Peer_ID;
+
+    HandShake_Message(int Peer_ID){
+        this.Peer_ID = Peer_ID;
+        String Header_Info = Header+Zero_Bits+Integer.toString(Peer_ID);
+        Msg = Header_Info.getBytes();
+
+
+    }
+
+}
